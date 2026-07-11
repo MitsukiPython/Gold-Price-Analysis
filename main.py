3a2e68cc9ab655cfd6a611adb1517b020a89ea82
Yume <109883910+MitsukiPython@users.noreply.github.com>
2026-07-11 13:35:52 +0000

    Testing new changes

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import requests
-import json
-import pandas
-import matplotlib
-import xgboost
+import requests as rq
+import json as js
+import pandas as pa
+import matplotlib as mpl
+import xgboost as xg
+import numpy as np
+import os
 
 
-def main(): 
+def main():
+  print("Hello!")
+  print("Did this get added to the og repo?")
+
+
+
+
+
 
 if __name__ == "__main__":
   main()
